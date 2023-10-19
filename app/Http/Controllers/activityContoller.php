388ce84a5bd425f25f21ad4f606fac3ee6ea2723
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-10-19 11:19:29 -0700

    FIRST COMMIT

---
+++ app/Http/Controllers/activityContoller.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Activity;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class activityContoller extends Controller
+{
+    public function index()
+    {
+        // HOME/WELCOME
+    }
+
+    public function create()
+    {
+        //NO
+    }
+
+    public function store(Request $request)
+    {
+        $activity_name = 
+    }
+
+    public function show(Activity $activity)
+    {
+        //
+    }
+
+    public function edit(Activity $activity)
+    {
+        //
+    }
+
+    public function update(Request $request, Activity $activity)
+    {
+        //
+    }
+
+    public function destroy(Activity $activity)
+    {
+        //
+    }
+}
